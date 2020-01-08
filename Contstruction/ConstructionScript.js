3c4aaebdc67baa20557a4b981e5e61759dbf483d
Kjetil F <kjefriborg@gmail.com>
2020-01-08 02:06:47 +0100

    Fixed smol bug

--- Contstruction/ConstructionScript.js
+++ Contstruction/ConstructionScript.js
@@ -96,7 +96,7 @@ function CalculateExp() {
     document.getElementById("TotalExp").innerHTML = "New total experience: " + NewTotalExp.toLocaleString();
 	
 	//Outputs new level and % to new level
-	if (CurrentExp + ExperienceGained => 200000) {
+	if (CurrentExp + ExperienceGained >= 200000) {
 		document.getElementById("NewConstructionLevel").innerHTML = "Congratulations! You reached 200M experience.";
 	} else {
 		var NewLevel = CalculateNewLevel(CurrentExp,ExperienceGained, "Construction");
